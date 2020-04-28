2d7ca5a16d24e067b4ff2fdd80a6c8130a301cde
Marcus Ravn Jensen <marcusravn1996'gmail.com>
2020-04-28 10:52:25 +0200

    beklædning

--- Fog/src/main/java/DBAccess/dbInit.sql
+++ Fog/src/main/java/DBAccess/dbInit.sql
@@ -2,91 +2,102 @@
 
 -- noinspection SqlNoDataSourceInspectionForFile
 DROP database if exists inventory;
-CREATE DATABASE  IF NOT EXISTS `inventory`;
+CREATE DATABASE  IF NOT EXISTS inventory;
 
 SET GLOBAL time_zone = '+00:00';
 
-USE `inventory`;
+USE inventory;
 
-DROP TABLE IF EXISTS `height`;
-CREATE TABLE `height` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `height`int(11) NOT NULL,
-  PRIMARY KEY (`id`)
+DROP TABLE IF EXISTS height;
+CREATE TABLE height (
+  id int(11) NOT NULL AUTO_INCREMENT,
+  heightint(11) NOT NULL,
+  PRIMARY KEY (id)
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
 
-DROP TABLE IF EXISTS `length`;
-CREATE TABLE `length` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `length`int(11) NOT NULL,
-  PRIMARY KEY (`id`)
+DROP TABLE IF EXISTS length;
+CREATE TABLE length (
+  id int(11) NOT NULL AUTO_INCREMENT,
+  lengthint(11) NOT NULL,
+  PRIMARY KEY (id)
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
 
-DROP TABLE IF EXISTS `width`;
-CREATE TABLE `width` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `width`int(11) NOT NULL,
-  PRIMARY KEY (`id`)
+DROP TABLE IF EXISTS width;
+CREATE TABLE width (
+  id int(11) NOT NULL AUTO_INCREMENT,
+  widthint(11) NOT NULL,
+  PRIMARY KEY (id)
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
 
-DROP TABLE IF EXISTS `slope`;
-CREATE TABLE `slope`(
- `id`int(11) NOT NULL AUTO_INCREMENT,
- `slope` int(11) NOT NULL,
-    PRIMARY KEY (`id`)
+DROP TABLE IF EXISTS slope;
+CREATE TABLE slope(
+ idint(11) NOT NULL AUTO_INCREMENT,
+ slope int(11) NOT NULL,
+    PRIMARY KEY (id)
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
 
-DROP TABLE IF EXISTS `roof_type`;
-CREATE TABLE `roof_type`(
- `id`int(11) NOT NULL AUTO_INCREMENT,
- `name` VARCHAR (100) NOT NULL,
-    PRIMARY KEY (`id`)
+DROP TABLE IF EXISTS roof_type;
+CREATE TABLE roof_type(
+ idint(11) NOT NULL AUTO_INCREMENT,
+ name VARCHAR (100) NOT NULL,
+    PRIMARY KEY (id)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
 
-DROP TABLE IF EXISTS `roof_color`;
-CREATE TABLE `roof_color`(
- `id`int(11) NOT NULL AUTO_INCREMENT,
- `color` VARCHAR (100) NOT NULL,
-    PRIMARY KEY (`id`)
+DROP TABLE IF EXISTS roof_color;
+CREATE TABLE roof_color(
+ idint(11) NOT NULL AUTO_INCREMENT,
+ color VARCHAR (100) NOT NULL,
+    PRIMARY KEY (id)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
 
-DROP TABLE IF EXISTS `roof`;
-CREATE TABLE `roof`(
- `id`int(11) NOT NULL AUTO_INCREMENT,
- `type_id` int(11) NOT NULL,
- `color_id` int (11) NOT NULL,
-    PRIMARY KEY (`id`),
-    foreign KEY (type_id)references `roof_type`(id),
-    foreign KEY (color_id)references `roof_color`(id)
+DROP TABLE IF EXISTS roof;
+CREATE TABLE roof(
+ idint(11) NOT NULL AUTO_INCREMENT,
+ type_id int(11) NOT NULL,
+ color_id int (11) NOT NULL,
+    PRIMARY KEY (id),
+    foreign KEY (type_id)references roof_type(id),
+    foreign KEY (color_id)references roof_color(id)
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
 
 
-DROP TABLE IF EXISTS `skur_length`;
-CREATE TABLE `skur_length` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `skur_length`int(11) NOT NULL,
-  PRIMARY KEY (`id`)
+DROP TABLE IF EXISTS skur_length;
+CREATE TABLE skur_length (
+  id int(11) NOT NULL AUTO_INCREMENT,
+  skur_lengthint(11) NOT NULL,
+  PRIMARY KEY (id)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
 
-DROP TABLE IF EXISTS `skur_width`;
-CREATE TABLE `skur_width` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `skur_width`int(11) NOT NULL,
-  PRIMARY KEY (`id`)
+DROP TABLE IF EXISTS skur_width;
+CREATE TABLE skur_width (
+  id int(11) NOT NULL AUTO_INCREMENT,
+  skur_widthint(11) NOT NULL,
+  PRIMARY KEY (id)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
 
-DROP table if exists `beklædning`;
-create table `beklædning` (
+DROP table if exists beklædning;
+create table beklædning (
  id int(11) NOT NULl AUTO_INCREMENT,
- `beklædning_type`VARCHAR(100) NOT NULL,
- PRIMARY KEY (`id`)
+ beklædning_typeVARCHAR(100) NOT NULL,
+ PRIMARY KEY (id)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
 
-DROP table if exists `floor`;
-create table `floor` (
+DROP table if exists floor;
+create table floor (
  id int(11) NOT NULl AUTO_INCREMENT,
- `floor_type`VARCHAR(100) NOT NULL,
- PRIMARY KEY (`id`)
+ floor_typeVARCHAR(100) NOT NULL,
+ PRIMARY KEY (id)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
+
+DROP table if exists wallCladding;
+create table wallCladding (
+ id int(11) NOT NULl AUTO_INCREMENT,
+ rightSide BOOL(1),
+ leftSide BOOL(1),
+ backing BOOL(1),
+ material_id int(11),
+ PRIMARY KEY (id),
+ FOREIGN KEY (material_id) beklædning(id)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
 
 
@@ -130,42 +141,42 @@ insert into roof(type_id,color_id)Values(3,5);
 
 
 
-INSERT INTO `slope`(slope)VALUES(15);
-INSERT INTO `slope`(slope)VALUES(20);
-INSERT INTO `slope`(slope)VALUES(25);
-INSERT INTO `slope`(slope)VALUES(30);
-INSERT INTO `slope`(slope)VALUES(35);
-INSERT INTO `slope`(slope)VALUES(40);
+INSERT INTO slope(slope)VALUES(15);
+INSERT INTO slope(slope)VALUES(20);
+INSERT INTO slope(slope)VALUES(25);
+INSERT INTO slope(slope)VALUES(30);
+INSERT INTO slope(slope)VALUES(35);
+INSERT INTO slope(slope)VALUES(40);
 
-INSERT INTO `height`(height)VALUES(180);
-INSERT INTO `height`(height)VALUES(190);
-INSERT INTO `height`(height)VALUES(200);
-INSERT INTO `height`(height)VALUES(210);
-INSERT INTO `height`(height)VALUES(220);
-INSERT INTO `height`(height)VALUES(230);
-INSERT INTO `height`(height)VALUES(240);
+INSERT INTO height(height)VALUES(180);
+INSERT INTO height(height)VALUES(190);
+INSERT INTO height(height)VALUES(200);
+INSERT INTO height(height)VALUES(210);
+INSERT INTO height(height)VALUES(220);
+INSERT INTO height(height)VALUES(230);
+INSERT INTO height(height)VALUES(240);
 
-INSERT INTO `length`(length)VALUES(240);
-INSERT INTO `length`(length)VALUES(290);
-INSERT INTO `length`(length)VALUES(340);
-INSERT INTO `length`(length)VALUES(390);
-INSERT INTO `length`(length)VALUES(340);
-INSERT INTO `length`(length)VALUES(390);
-INSERT INTO `length`(length)VALUES(440);
-INSERT INTO `length`(length)VALUES(490);
-INSERT INTO `length`(length)VALUES(540);
-INSERT INTO `length`(length)VALUES(590);
-INSERT INTO `length`(length)VALUES(640);
-INSERT INTO `length`(length)VALUES(690);
-INSERT INTO `length`(length)VALUES(740);
+INSERT INTO length(length)VALUES(240);
+INSERT INTO length(length)VALUES(290);
+INSERT INTO length(length)VALUES(340);
+INSERT INTO length(length)VALUES(390);
+INSERT INTO length(length)VALUES(340);
+INSERT INTO length(length)VALUES(390);
+INSERT INTO length(length)VALUES(440);
+INSERT INTO length(length)VALUES(490);
+INSERT INTO length(length)VALUES(540);
+INSERT INTO length(length)VALUES(590);
+INSERT INTO length(length)VALUES(640);
+INSERT INTO length(length)VALUES(690);
+INSERT INTO length(length)VALUES(740);
 
-INSERT INTO `width`(width)VALUES(240);
-INSERT INTO `width`(width)VALUES(280);
-INSERT INTO `width`(width)VALUES(320);
-INSERT INTO `width`(width)VALUES(360);
-INSERT INTO `width`(width)VALUES(400);
-INSERT INTO `width`(width)VALUES(440);
-INSERT INTO `width`(width)VALUES(480);
-INSERT INTO `width`(width)VALUES(520);
-INSERT INTO `width`(width)VALUES(560);
-INSERT INTO `width`(width)VALUES(600);
+INSERT INTO width(width)VALUES(240);
+INSERT INTO width(width)VALUES(280);
+INSERT INTO width(width)VALUES(320);
+INSERT INTO width(width)VALUES(360);
+INSERT INTO width(width)VALUES(400);
+INSERT INTO width(width)VALUES(440);
+INSERT INTO width(width)VALUES(480);
+INSERT INTO width(width)VALUES(520);
+INSERT INTO width(width)VALUES(560);
+INSERT INTO width(width)VALUES(600);
--- Fog/src/main/webapp/index.jsp
+++ Fog/src/main/webapp/index.jsp
@@ -7,6 +7,7 @@
     <div class="form-group col-lg-5 mt-5"></div>
     <div class="form-group col-lg-2 mt-5">
         <div class="form-group mt-4">
+            Højde
             <select required class="form-control number-input" id="height" name="height"
                     onchange="saveToStorage('height', document.getElementById('height').value)">
                 <option value="" disabled selected>Højde</option>
@@ -143,31 +144,69 @@
             </select>
         </div>
     </div>
+
     <div class="form-group col-lg-5 mt-5">
+        <div class="form-group mt-5">
+            Ønskes der beklædning på siderne af carporten?
+            <label class="container">Ja
+                <input type="checkbox" id="myCheckBacking" onclick="myFunctionSkur()">
+                <span class="checkmark"></span>
+            </label>
+        </div>
 
-        ${requestScope.svgdrawing}
+        <div class="form-group mt-5">
+            <div class="form-check">
+                <input type="checkbox" class="form-check-input" id="rightCheck">
+                <label class="form-check-label" for="rightCheck">Højre</label>
+            </div>
+            <div class="form-check">
+                <input type="checkbox" class="form-check-input" id="leftCheck">
+                <label class="form-check-label" for="leftCheck">Venstre</label>
+            </div>
+            <div class="form-check">
+                <input type="checkbox" class="form-check-input" id="backCheck">
+                <label class="form-check-label" for="backCheck">Bagvæggen</label>
+            </div>
+        </div>
 
+        <div class="form-group mt-5">
+            <select required class="form-control number-input" id="backingWood" name="backingWood"
+            <option value="" disabled selected>Beklædning til sider</option>
+            <c:forEach var="i" items="${DimensionsFacade.getAllWoodPanels()}" varStatus="Count">
+                <option value="${Count.index+1}">
+                        ${i}
+                </option>
+            </c:forEach>
+            </select>
+        </div>
     </div>
 
+    <%--
+                <div class="form-group col-lg-5 mt-5">
 
-</div>
+                    ${requestScope.svgdrawing}
 
-
-<form action="FrontController" method="post" id="valueSender">
-    <input type="hidden" id="target" name="taget" value="widthSetter">
-    <input type="hidden" id="senderWidth" name="senderWidth" value="">
-    <input type="hidden" id="senderLength" name="senderLength" value="">
-</form>
-
-<script>load()</script>
-
-<script>loadInput("slope", 'myCheckTag')</script>
-<script>loadInput("roofType", 'myCheckTag')</script>
-<script>loadInput("shedLength", 'myCheckSkur')</script>
-<script>loadInput("shedWidth", 'myCheckSkur')</script>
-<script>loadInput("shedWood", 'myCheckSkur')</script>
-<script>loadInput("shedFloor", 'myCheckSkur')</script>
+                </div>
 
 
+            </div>
 
-<%@include file="Includes/Footer.inc" %>
+
+            <form action="FrontController" method="post" id="valueSender">
+                <input type="hidden" id="target" name="taget" value="widthSetter">
+                <input type="hidden" id="senderWidth" name="senderWidth" value="">
+                <input type="hidden" id="senderLength" name="senderLength" value="">
+            </form>
+
+            <script>load()</script>
+
+            <script>loadInput("slope", 'myCheckTag')</script>
+            <script>loadInput("roofType", 'myCheckTag')</script>
+            <script>loadInput("shedLength", 'myCheckSkur')</script>
+            <script>loadInput("shedWidth", 'myCheckSkur')</script>
+            <script>loadInput("shedWood", 'myCheckSkur')</script>
+            <script>loadInput("shedFloor", 'myCheckSkur')</script>
+    --%>
+
+
+    <%@include file="Includes/Footer.inc" %>
