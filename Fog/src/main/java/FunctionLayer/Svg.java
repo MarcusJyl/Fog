df1a5c2b6fadd16347c63ba58b3f0ce0817e29f3
Daniel <daniel.poulsen@live.dk>
2020-05-11 10:18:29 +0200

    SVG

--- Fog/src/main/java/FunctionLayer/Svg.java
+++ Fog/src/main/java/FunctionLayer/Svg.java
@@ -9,22 +9,31 @@ public class Svg {
     private String viewbox;
     private int x;
     private int y;
+    private int x2;
+    private int y2;
     private StringBuilder svg = new StringBuilder();
 
     private final String headerTemplate = "<svg version=\"1.1\" xmlns=\"http://www.w3.org/2000/svg\" xmlns:xlink=\"http://www.w3.org/1999/xlink\" height=\"%s\" width=\"%s\" viewBox=\"%s\" preserveAspectRatio=\"xMinYMin\">";
     private final String rectTemplate = "<rect x=\"%d\" y=\"%d\" height=\"%d\" width=\"%d\" style=\"stroke:#000000; fill: #ffffff\" />";
+    private final String crossTemplate = "<line x=\"%d\" y=\"%d\" x2=\"%d\" y2=\"%d\" style=\"stroke:#000000; stroke-dasharray: 5 5;\" />";
 
-    public Svg(int width, int height, String viewbox, int x, int y) {
+    public Svg(int width, int height, String viewbox, int x, int y, int x2, int y2) {
         this.width = width;
         this.height = height;
         this.viewbox = viewbox;
         this.x = x;
         this.y = y;
+        this.x2 = x2;
+        this.y2 = y2;
         svg.append(String.format(headerTemplate, height, width, viewbox));
     }
 
     public void addRect(int x, int y, int height, int width){
         svg.append(String.format(rectTemplate, x, y, height, width));
+    }
+
+    public void addCross(int x, int y, int x2, int y2){
+        svg.append(String.format(crossTemplate, x, y, x2, y2));
     }
 
     public int getWidth() {
@@ -51,9 +60,7 @@ public class Svg {
         this.viewbox = viewbox;
     }
 
-    public int getX() {
-        return x;
-    }
+    public int getX() { return x; }
 
     public void setX(int x) {
         this.x = x;
@@ -66,6 +73,14 @@ public class Svg {
     public void setY(int y) {
         this.y = y;
     }
+
+    public int getX2() { return x2; }
+
+    public void setX2(int x2){this.x2 = x2; }
+
+    public int gety2() { return y2; }
+
+    public void sety2(int y2){this.y2 = y2; }
 
     @Override
     public String toString() {
--- Fog/src/main/java/PresentationLayer/Drawing.java
+++ Fog/src/main/java/PresentationLayer/Drawing.java
@@ -12,6 +12,12 @@ public class Drawing extends Command {
     public String execute(HttpServletRequest request, HttpServletResponse response) throws LoginSampleException {
         int width = Integer.parseInt(request.getParameter("senderWidth"));
         int length = Integer.parseInt(request.getParameter("senderLength"));
+        int rem = width - 35;
+        int krydsY = rem + 4;
+        int krydsX = length - 55;
+        int stolpe = width - 35;
+
+
 
 
         HttpSession session = request.getSession();
@@ -19,8 +25,24 @@ public class Drawing extends Command {
         session.setAttribute("maxWidth", width);
 
 
-        Svg svg = new Svg(800, 600, "0,0,800,600", 0, 0);
-        svg.addRect(0, 0, length, width);
+        Svg svg = new Svg(800, 600, "0,0,800,600", 0, 0, 0, 0);
+        svg.addRect(0, 0, width, length);
+        //Remme
+        svg.addRect(0, 35, 4, length);
+        svg.addRect(0, rem, 4, length);
+        //Spær
+        svg.addRect(0,0,width,4);
+        svg.addRect(length, 0, width, 4);
+        //Kryds
+        svg.addCross(55, 35, krydsX, krydsY);
+        svg.addCross(55, krydsY, krydsX, 35);
+        //Stolper
+        svg.addRect(110, 32, 10,10);
+        svg.addRect(110, stolpe, 10,10);
+
+
+
+
 
         request.setAttribute("svgdrawing", svg.toString());
         return "../index";
