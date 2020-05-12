cbebcc391e98c2f7bc6baad521673befdec34a82
Marc <marc.g.ekstrom@gmail.com>
2020-05-12 17:16:46 +0200

    SVG

--- Fog/src/main/java/FunctionLayer/Svg.java
+++ Fog/src/main/java/FunctionLayer/Svg.java
@@ -4,8 +4,8 @@ import com.sun.javafx.binding.StringFormatter;
 
 public class Svg {
 
-    private int width;
-    private int height;
+    private int width1;
+    private int height1;
     private String viewbox;
     private int x;
     private int y;
@@ -14,12 +14,12 @@ public class Svg {
     private StringBuilder svg = new StringBuilder();
 
     private final String headerTemplate = "<svg version=\"1.1\" xmlns=\"http://www.w3.org/2000/svg\" xmlns:xlink=\"http://www.w3.org/1999/xlink\" height=\"%s\" width=\"%s\" viewBox=\"%s\" preserveAspectRatio=\"xMinYMin\">";
-    private final String rectTemplate = "<rect x=\"%d\" y=\"%d\" height=\"%f\" width=\"%d\" style=\"stroke:#000000; fill: #ffffff\" />";
+    private final String rectTemplate = "<rect x=\"%d\" y=\"%d\" height=\"%f\" width=\"%f\" style=\"stroke:#000000; fill: #ffffff\" />";
     private final String crossTemplate = "<line x1=\"%d\" y1=\"%d\" x2=\"%d\" y2=\"%d\" style=\"stroke:#000000; stroke-dasharray: 5 5;\" />";
 
     public Svg(int width, int height, String viewbox, int x, int y, int x2, int y2) {
-        this.width = width;
-        this.height = height;
+        this.width1 = width;
+        this.height1 = height;
         this.viewbox = viewbox;
         this.x = x;
         this.y = y;
@@ -28,7 +28,7 @@ public class Svg {
         svg.append(String.format(headerTemplate, height, width, viewbox));
     }
 
-    public void addRect(int x, int y, double height, int width){
+    public void addRect(int x, int y, double height, double width){
         svg.append(String.format(rectTemplate, x, y, height, width));
     }
 
@@ -37,20 +37,28 @@ public class Svg {
         svg.append(String.format(crossTemplate, x, y, x2, y2));
     }
 
+//    public void addSpær(int y, double length, int width){
+//        for(int i = 0; i < length; i+=55){
+//            svg.append(String.format(rectTemplate, i, y, length, width));
+//
+//        }
+//    }
+
+
     public int getWidth() {
-        return width;
+        return width1;
     }
 
     public void setWidth(int width) {
-        this.width = width;
+        this.width1 = width;
     }
 
     public int getHeight() {
-        return height;
+        return height1;
     }
 
     public void setHeight(int height) {
-        this.height = height;
+        this.height1 = height;
     }
 
     public String getViewbox() {
--- Fog/src/main/java/PresentationLayer/Drawing.java
+++ Fog/src/main/java/PresentationLayer/Drawing.java
@@ -12,12 +12,15 @@ public class Drawing extends Command {
     public String execute(HttpServletRequest request, HttpServletResponse response) throws LoginSampleException {
         int width = Integer.parseInt(request.getParameter("senderWidth"));
         int length = Integer.parseInt(request.getParameter("senderLength"));
-        int rem = width - 35;
+        int rem = length - 35;
         int krydsY = rem + 4;
-        int krydsX = length - 55;
-        int stolpe = width - 35;
+        int krydsX = width - 55;
+        int stolpe = length - 35;
 
+        double lineWidth = 4.5;
+        System.out.println(length);
 
+        int StopleY = 35;
 
 
         HttpSession session = request.getSession();
@@ -26,15 +29,18 @@ public class Drawing extends Command {
 
 
         Svg svg = new Svg(800, 600, "0,0,800,600", 0, 0, 0, 0);
-        svg.addRect(0, 0, width, length);
+        svg.addRect(0, 0, length, width);
         //Remme
-        svg.addRect(0, 35, 4.5, length);
-        svg.addRect(0, rem, 4, length);
+        svg.addRect(0, StopleY, lineWidth, width );
+        svg.addRect(0, rem, lineWidth, width);
         //Spær
-        svg.addRect(0,0,width,4);
-        svg.addRect(length, 0, width, 4);
+        for(int i = 0; i < width; i+= 55){
+            svg.addRect(i, 0, length , lineWidth);
+        }
+//        svg.addRect(0,0,width,4);
+//        svg.addRect(length, 0, width, 4);
         //Kryds
-        svg.addCross(55, 35, krydsX, krydsY);
+        svg.addCross(55, StopleY, krydsX, krydsY);
         svg.addCross(55, krydsY, krydsX, 35);
         //Stolper
         svg.addRect(110, 32, 10,10);
